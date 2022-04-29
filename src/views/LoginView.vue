29b42bdd018d8bf31ab69bdd003a2329bfde159b
holgerpart <Hobevalge1>
2022-04-29 15:47:43 +0300

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input type="text" placeholder="Kasutajanimi" v-model="userName">
+    <br>
+    <br>
+    <input type="text" placeholder="Salasõna" v-model="password">
+    <br>
+    <br>
+    <button v-on:click="login">Sisene</button>
+
+  </div>
+</template>
+<!--{-->
+<!--"userName": "string",-->
+<!--"password": "string"-->
+<!--}-->
+<script>
+export default {
+  name: "LoginView",
+  data: function () {
+    return {
+      userName: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    login: function () {
+      let someDtoObject = {
+        userName: this.userName,
+        password: this.password
+      }
+
+      // if using alternative then remove "this." from someDtoObject
+      this.$http.post("/log-in", someDtoObject
+      ).then(response => {
+        alert(response.data.userId)
+        alert(response.data.userRoleIds)
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

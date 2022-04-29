113b88cbbba4b4329f482d2bfe4b94981436c86f
holgerpart <Hobevalge1>
2022-04-29 21:20:30 +0300

    Registreerimise sisestused

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,12 +1,61 @@
 <template>
 <div>
-
+  <input type="text" placeholder="Eesnimi" v-model="firstName">
+  <br>
+  <br>
+  <input type="text" placeholder="Perekonnanimi" v-model="lastName">
+  <br>
+  <br>
+  <input type="text" placeholder="Kasutajanimi" v-model="name">
+  <br>
+  <br>
+  <input type="text" placeholder="Salasõna" v-model="password">
+  <br>
+  <br>
+  <select v-model="role">
+    <option disabled value="">Valige roll</option>
+    <option>user</option>
+    <option>shop</option>
+  </select>
+  <button v-on:click="register">Salvesta andmed</button>
+  <br>
+  <br>
 </div>
 </template>
-
+<!--"firstName": "string",-->
+<!--"lastName": "string",-->
+<!--"name": "string",-->
+<!--"password": "string",-->
+<!--"role": "string"-->
 <script>
 export default {
-  name: "RegisterView"
+  name: "RegisterView",
+  data: function () {
+    return {
+      firstName: '',
+      lastName: '',
+      name: '',
+      password: '',
+      role: ''
+    }
+  },
+  methods: {
+    register: function () {
+      let registryRequest = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        name: this.name,
+        password: this.password,
+        role: this.role
+      }
+      this.$http.post("/log-in/user", registryRequest
+      ).then(response => {
+        alert("Success")
+          }).catch(error => {
+            alert("Error")
+      })
+    }
+  }
 }
 </script>
 

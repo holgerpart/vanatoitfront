d7a4aad5d440089cd342d7c60849f8bc1a5d1e4f
holgerpart <Hobevalge1>
2022-05-14 21:59:24 +0300

    Addshopview

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -9,27 +9,57 @@
       <button class="nav-button" @click="navigateToOrders">Tellimused</button>
       <button class="nav-button" @click="navigateToLogin">Logi välja</button>
     </div>
-    <div class="shop">
-      <h3>Vali pood</h3>
-    </div>
-    <div v-if="true">
+    <div v-if="selectionDisplay">
+      <div class="shop">
+        <h3>Vali pood</h3>
+      </div>
+
+      <div v-if="true">
         <ul>
           <li class="radio" v-for="shop in shops">
             <input v-on:change="selectName(shop.shopName)" type="radio" v-model="shopId"
                    :value="shop.shopId">{{ shop.shopName }}
           </li>
         </ul>
+      </div>
+
+
+      <button class="small-button" v-on:click="navigateToStockInput">Sisesta uus kaubarida
+      </button>
+      <br>
+      <br>
+      <button class="small-button" v-on:click="navigateToStock">Vaata laoseisu</button>
+      <br>
+      <br>
+      <button class="small-button" v-on:click="navigateToOrders">Vaata tellimusi</button>
+      <br>
+      <br>
+      <button class="small-button" v-on:click="addArticleView">Lisa uus artikkel</button>
+      <br>
+      <br>
+      <button class="small-button" v-on:click="addTypeView">Lisa uus kategooria</button>
     </div>
+    <div v-if="articleDisplay">
+      <div class="shop">
+        <h3>Lisa uus artikkel</h3>
+      </div>
+      <input class="select" type="text" placeholder="Toiduartikli nimi" v-model="articleName">
+      <input class="select" type="text" placeholder="Kategooria" v-model="typeName">
+      <br>
+      <button class="small-button" v-on:click="addNewArticle">Lisa</button>
+      <button class="small-button" v-on:click="addArticleView">Tühista</button>
 
+    </div>
+    <div v-if="typeDisplay">
+      <div class="shop">
+        <h3>Lisa uus kategooria</h3>
+      </div>
+      <input class="select" type="text" placeholder="Kategooria nimi" v-model="typeName">
+      <br>
+      <button class="small-button" v-on:click="addNewType">Lisa</button>
+      <button class="small-button" v-on:click="addTypeView">Tühista</button>
 
-    <button class="small-button" v-on:click="navigateToStockInput">Sisesta uus kaubarida
-    </button>
-    <br>
-    <br>
-    <button class="small-button" v-on:click="navigateToStock">Vaata laoseisu</button>
-    <br>
-    <br>
-    <button class="small-button" v-on:click="navigateToOrders">Vaata tellimusi</button>
+    </div>
   </div>
 </template>
 
@@ -43,8 +73,11 @@ export default {
       shopId: sessionStorage.getItem('shopId'),
       roleId: sessionStorage.getItem('roleId'),
       shopName: sessionStorage.getItem('shopName'),
-
-
+      selectionDisplay: true,
+      articleName: '',
+      typeName: '',
+      articleDisplay: false,
+      typeDisplay: false,
     }
   },
   methods: {
@@ -66,6 +99,58 @@ export default {
       }).catch(error => {
         console.log(error)
       })
+    },
+    addNewArticle: function () {
+      // ALTERNATIVE
+      let request = {
+        articleName: this.articleName,
+        typeName: this.typeName,
+      }
+
+      // if using alternative then remove "this." from someDtoObject
+      this.$http.post("/stock/article", request
+      ).then(response => {
+        console.log(response.data)
+        this.addArticleView()
+      }).catch(error => {
+        console.log(error)
+        alert(error.response.data.detail)
+      })
+    },
+    addNewType: function () {
+      this.$http.get("/stock/type", {
+            params: {
+              typeName: this.typeName
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+        this.addTypeView()
+      }).catch(error => {
+        console.log(error)
+        alert(error.response.data.detail)
+      })
+    },
+    // addNewType: function () {
+    //   let request = {
+    //     typeName: this.typeName
+    //   }
+    //   this.$http.get("/stock/type", request
+    //   ).then(response => {
+    //     console.log(response.data)
+    //     this.addTypeView()
+    //   }).catch(error => {
+    //     console.log(error)
+    //     alert(error.response.data.detail)
+    //   })
+    // },
+    addArticleView: function () {
+      this.selectionDisplay = !this.selectionDisplay
+      this.articleDisplay = !this.articleDisplay
+    },
+    addTypeView: function () {
+      this.selectionDisplay = !this.selectionDisplay
+      this.typeDisplay = !this.typeDisplay
     },
     navigateToAdmin: function () {
       sessionStorage.setItem('shopId', this.shopId)
